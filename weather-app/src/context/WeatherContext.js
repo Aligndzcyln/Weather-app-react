c1e46511c239f61153277b61a2f41d79425c37f2
Aligndzcyln <ceylanali80@gmail.com>
2021-08-29 17:10:20 +0300

    Weather data available for every city

--- weather-app/src/context/WeatherContext.js
+++ weather-app/src/context/WeatherContext.js
@@ -4,25 +4,27 @@ import axios from "axios";
 
 const WeatherContext = createContext();
 const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
-let data = [];
+
 
 export const WeatherProvider = ({ children }) => {
     const [weather, setWeather] = useState();
     const { form, setForm } = useForm();
 
     useEffect(() => {
-        data = axios(
-            `https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=alerts&units=metric&appid=${API_KEY}`
+        axios(
+            `https://api.openweathermap.org/data/2.5/onecall?lat=${form.lat}&lon=${form.lon}&exclude=alerts&units=metric&appid=${API_KEY}`
         )
             .then((res) => setWeather(res.data.daily))
             .catch((e) => console.log(e));
+
+        console.log(weather)
     }, [form])
 
     const values = {
         weather,
         setWeather,
     };
-    console.log(weather)
+
     return <WeatherContext.Provider value={values}>{children}</WeatherContext.Provider>
 
 }
