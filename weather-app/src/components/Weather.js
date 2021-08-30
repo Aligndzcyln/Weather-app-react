576e914743e38166a7a16bc9d42f70d1b0c23c83
Aligndzcyln <ceylanali80@gmail.com>
2021-08-30 16:33:39 +0300

    Added weather images and and displayed all forecasts

--- weather-app/src/components/Weather.js
+++ weather-app/src/components/Weather.js
@@ -4,11 +4,47 @@ import { useWeather } from "../context/WeatherContext";
 function Weather() {
   const { weather, setWeather } = useWeather();
 
+  const weatherSvg = {
+    Clear: <i className="fas fa-sun"></i>,
+    Clouds: <i className="fas fa-cloud"></i>,
+    Rain: <i className="fas fa-cloud-showers-heavy"></i>,
+    Snow: <i className="fas fa-snowflake"></i>,
+  };
+
+  const card = {
+    description: "",
+    main: "",
+  };
+
   return (
-    <div>
-      {weather.map((item, index) => {
-        return item["weather"][0]["main"];
-      })}
+    <div className="container">
+      <div className="row">
+        {weather.map((item, index) => {
+          card.description = item["weather"][0]["description"];
+          card.main = item["weather"][0]["main"];
+          console.log(card.description);
+          return (
+            <div key={index} className="col-sm-3 col-md-2">
+              <div className="card border-success mb-3">
+                <div className="card-header bg-transparent border-success">
+                  Day
+                </div>
+                <div className="card-body text-success">
+                  <h5 className="card-title">{card.main}</h5>
+                  <p className="card-text">
+                    <div style={{ fontSize: 120 }}>{weatherSvg[card.main]}</div>
+
+                    {card.description}
+                  </p>
+                </div>
+                <div className="card-footer bg-transparent border-success">
+                  {item["temp"]["day"]}
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
