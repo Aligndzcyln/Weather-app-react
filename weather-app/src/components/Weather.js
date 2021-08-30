ff5a489997e6b359ae0c6f007e3065d53f7a7d94
Aligndzcyln <ceylanali80@gmail.com>
2021-08-30 21:12:56 +0300

    Styled Weather component

--- weather-app/src/components/Weather.js
+++ weather-app/src/components/Weather.js
@@ -5,10 +5,15 @@ function Weather() {
   const { weather, setWeather } = useWeather();
 
   const weatherSvg = {
-    Clear: <i className="fas fa-sun"></i>,
-    Clouds: <i className="fas fa-cloud"></i>,
-    Rain: <i className="fas fa-cloud-showers-heavy"></i>,
-    Snow: <i className="fas fa-snowflake"></i>,
+    Clear: <i style={{ color: "#fcc203" }} className="fas fa-sun"></i>,
+    Clouds: <i style={{ color: "#949494" }} className="fas fa-cloud"></i>,
+    Rain: (
+      <i
+        style={{ color: "#949494" }}
+        className="fas fa-cloud-showers-heavy"
+      ></i>
+    ),
+    Snow: <i style={{ color: "#d9d9d9" }} className="fas fa-snowflake"></i>,
   };
 
   const card = {
@@ -25,11 +30,9 @@ function Weather() {
           console.log(card.description);
           return (
             <div key={index} className="col-sm-3 col-md-2">
-              <div className="card border-success mb-3">
-                <div className="card-header bg-transparent border-success">
-                  Day
-                </div>
-                <div className="card-body text-success">
+              <div className="card border-0 mb-3">
+                <div className="card-header bg-transparent border-0">Day</div>
+                <div className="card-body ">
                   <h5 className="card-title">{card.main}</h5>
                   <p className="card-text">
                     <div style={{ fontSize: 120 }}>{weatherSvg[card.main]}</div>
@@ -37,7 +40,7 @@ function Weather() {
                     {card.description}
                   </p>
                 </div>
-                <div className="card-footer bg-transparent border-success">
+                <div className="card-footer bg-transparent border-0">
                   {item["temp"]["day"]}
                 </div>
               </div>
